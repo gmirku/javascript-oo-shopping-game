eeeb4a0e3f94e22ebbd5ebafd81fd9f422c4b3fd
gmirku <76975353+gmirku@users.noreply.github.com>
2021-01-14 16:34:34 +0100

    module1

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -35,41 +35,42 @@ const dateDiff = (date1, date2) => {
 };
 
 // Here, use Object.defineProperty to create property - daysToExpire
-Object.defineProperty(Product,'daysToExpire',{ get(){
+Object.defineProperty(Product.prototype,'daysToExpire',{ get: function(){
   return dateDiff(this.expiryDate,new Date());
 }});
 // Add method getDetails to Product here
-Product.prototype.getDetails=function(){
-console.log(`Product Name: ${this.name} , Product Price: ${this.price}`);
+Product.prototype.getDetails = function(){
+  msg = `Product Name: ${this.name} , Product Price: ${this.price}`;
+  return msg;
+
 }
 // Define the MagicProduct class here
-const MagicProduct = {
 
-  MagicProduct(id,name,price,expiryDate,points,isBonus){
+function  MagicProduct(id,name,price,expiryDate,points,isBonus){
     Product.call(this,id,name,price,expiryDate);
-    this.points = 0;
-    this.isBonus = 0;
+    this.points = points;
+    this.isBonus = isBonus;
   }
-};
+
 
 // Establish inheritance between Product() & MagicProduct() here
 MagicProduct.prototype = Object.create(Product.prototype);
 // Define Rating class here
-class Rating{
-constructor(){
-  rate = "";
-}
-set rating(value){
-if (value >=1 && value < 10) { if(value > 1 && value <= 4)
-    rate = "OK";
-  else if(value <= 7)
-    rate = "GOOD";
-  else {
-    rate = "BAD";
-  }
-}
-}
-}
+class Rating {
+  constructor() {
+  this.rate = ""; }
+
+set rating(value) {
+   if (value > 1 && value <= 4)
+    this.rate = "OK";
+  else if(value >=5 && value <= 7)
+    this.rate = "GOOD";
+  else if(value > 7)
+    this.rate = "EXCEPTIONAL";
+  else
+    this.rate = "BAD";
+
+  }}
 // Complete the loadProducts function
 const loadProducts = (map, prodId) => {
     let a = new Array();
